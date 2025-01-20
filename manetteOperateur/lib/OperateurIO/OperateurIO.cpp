07f9b02d2508762adf3a40dcd4241eb28e510a61
Huskies Prog <huskies5439@polych.ca>
2025-01-20 16:29:06 -0500

    Clarification pointeur

--- manetteOperateur/lib/OperateurIO/OperateurIO.cpp
+++ manetteOperateur/lib/OperateurIO/OperateurIO.cpp
@@ -3,17 +3,16 @@
 
 /* Voici le site qui a permit de faire fonctionner le joystick
 https://stackoverflow.com/questions/76930785/c-arduino-pass-one-objects-instance-as-a-pointer-to-another-class
-
-Mais
-1) On peut écrire Joystick_* joystick (stackoverflow) OU Joystick_ *joystick (doc arduino) pour indiquer un pointeur et ça ne change rien ! Pourquoi ?
-2) D'où vient la syntaxe ": _joystick(joystick)" dans le constructeur ?? J'ai fait la même chose pour la manette du Qbot ? Où est la doc pour ça ?
-3) pourquoi joystick->setButton ? C'est un opérateur lambda ? On passe la fonction d'une classe à une autre ?
 */
 
-OperateurIO::OperateurIO(int firstPin, int lastPin, Joystick_ *joystick, Adafruit_NeoPixel strip) : _joystick(joystick) {
+//TODO : Mieux commenter la classe
+
+//Le * peut être soit collé sur le type, soit sur le nom de la variable.
+OperateurIO::OperateurIO(int firstPin, int lastPin, Joystick_ *joystick, Adafruit_NeoPixel strip) {
   _firstPin = firstPin;
   _lastPin = lastPin;
   _strip = strip;
+  _joystick = joystick; //pas mettre le * car le nom de la variable du pointeur est seulement joystick, pas *joystick.
   _dernierBouton = 0;
   _dernierBoutonPasse = 0;
 }
@@ -35,7 +34,7 @@ void OperateurIO::loopBoutonEtLED() {
     for (int i = _firstPin; i <= _lastPin; i++){
       
       if (i == _dernierBouton) {//Allume le dernier bouton
-        _joystick -> setButton(i, 1);
+        _joystick -> setButton(i, 1); //Il faut utiliser l'opérateur lambda -> car on passe par un pointeur 
         _strip.setPixelColor(i-2, 0, 255, 0);
 
       } else {//Éteint tous les autres
