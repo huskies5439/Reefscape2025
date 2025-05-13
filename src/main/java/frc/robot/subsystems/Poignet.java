3798b39580bfb25146955c5e2630cd10f92b400d
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2025-05-13 13:18:30 -0400

    kp etais pas correct, mais ça fonctionne!!!!!!!!!!!!!!

--- src/main/java/frc/robot/subsystems/Poignet.java
+++ src/main/java/frc/robot/subsystems/Poignet.java
@@ -29,7 +29,7 @@ public class Poignet extends SubsystemBase {
   private DigitalInput limitSwitch = new DigitalInput(7);
 
   // PID + FeedForward
-  private ProfiledPIDController pidPoignet = new ProfiledPIDController(0.0013202, 0, 0, // kp = 0.045
+  private ProfiledPIDController pidPoignet = new ProfiledPIDController(0.045, 0, 0, // kp = 0.045
       new TrapezoidProfile.Constraints(540, 540));
 
   private ArmFeedforward feedforward = new ArmFeedforward(0.18635, 0.58213, 0.00085991,0.00047057); // 0.1564, 0.1749, 0.01049,0
